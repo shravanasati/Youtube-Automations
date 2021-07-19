efe04935d059903b0db9d3410c7cb73785983a79
Shravan Asati <dev.shravan@protonmail.com>
2021-07-19 19:03:01 +0530

    🚧 progress bars for download stats

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,6 +1,6 @@
 from pytube import YouTube, Playlist
 import validators
-from os import getcwd, chdir, getlogin, rename, mkdir
+from os import getcwd, chdir, rename, mkdir
 from os.path import join as join_path, getsize, exists, expanduser
 from multiprocessing import Process
 from time import sleep
@@ -8,11 +8,14 @@ import re
 from shutil import move as move_file
 from string import ascii_letters as alphabets
 from random import choice as random_choice
+from tqdm import tqdm
+
 
 class YouTubeDownloader():
     """
     Interface to download youtube videos using pytube module.
     """
+
     def __init__(self, video_url, type, filepath=None):
         """
         video_url = The URL of the Youtube video.
@@ -52,7 +55,7 @@ class YouTubeDownloader():
             self.filepath = getcwd()
 
     @staticmethod
-    def index_of_key(key, d:dict):
+    def index_of_key(key, d: dict):
         """
         Returns the index of the key from the dictionary.
         """
@@ -62,7 +65,9 @@ class YouTubeDownloader():
                 index = i
                 return index
 
-    def show_progress(self, filesize):
+        return -1
+
+    def show_progress(self, filesize: int):
         """
         Shows the download progress and changes extension to mp3.
         """
@@ -72,18 +77,21 @@ class YouTubeDownloader():
         # * waiting for seconds until the file is created because sometimes the file is not created and then it results in FileNotFoundError
         while not exists(original_path):
             sleep(1)
-        
+
         # * showing the download progress
         size_on_disk = getsize(original_path)
-        while size_on_disk < filesize:
-            size_on_disk = getsize(original_path)
-            ratio = (size_on_disk / filesize) * 100
-            print(f"{ratio:.2f}% download completed ({size_on_disk/1000000}MB/{filesize/1000000}MB).")
-            sleep(5)
+
+        with tqdm(total=100) as pbar:
+            while size_on_disk < filesize:
+                size_on_disk = getsize(original_path)
+                ratio = (size_on_disk / filesize) * 100
+                # print(f"{ratio:.2f}% download completed ({size_on_disk/1000000}MB/{filesize/1000000}MB).")
+                pbar.update(float(f"{ratio:.2f}"))
 
         # changing extension to mp3 if audio
         if self.type == "audio":
-            rename(original_path, join_path(getcwd(), self.video_title + ".mp3"))
+            rename(original_path, join_path(
+                getcwd(), self.video_title + ".mp3"))
             self.format = ".mp3"
 
     def move_after_download(self):
@@ -92,17 +100,16 @@ class YouTubeDownloader():
         """
         if getcwd() == self.filepath:
             return None
-            
+
         try:
             original_path = join_path(getcwd(), self.video_title + self.format)
-            
+
             # * moving the file to self.filepath
             move_file(original_path, self.filepath)
             print("File moved to", self.filepath)
 
         except Exception as e:
             print(e)
-
 
     def download_audio(self):
         """
@@ -132,15 +139,15 @@ class YouTubeDownloader():
         except Exception as e:
             print("Following error occured:", e)
             quit()
-        
 
-    def download_video(self, resolution:str="720p"):
+    def download_video(self, resolution: str = "720p"):
         """
         Downloads the given youtube video in video format.
         """
         try:
-            # * itags for different resolutions for mp4 downloads 
-            resolutions = {"2160p":313 ,"1440p":271, "1080p":299, "720p":22, "480p":135, "360p":18, "240p":133, "144p":160}
+            # * itags for different resolutions for mp4 downloads
+            resolutions = {"2160p": 313, "1440p": 271, "1080p": 299,
+                           "720p": 22, "480p": 135, "360p": 18, "240p": 133, "144p": 160}
 
             # * getting the stream for the requested resolution
             stream = self.yt.streams.get_by_itag(resolutions[resolution])
@@ -153,7 +160,8 @@ class YouTubeDownloader():
                     print("No video stream available for this video!")
                     quit()
                 next_res = list(resolutions.keys())[next_index]
-                print(f"{resolution} resolution not available for this video, switching to {next_res} resolution.")
+                print(
+                    f"{resolution} resolution not available for this video, switching to {next_res} resolution.")
 
                 # * fetching the new stream until the resolution is correct
                 stream = self.yt.streams.get_by_itag(resolutions[next_res])
@@ -161,7 +169,6 @@ class YouTubeDownloader():
                     print("\n")
                     break
                 resolution = next_res
-
 
             download_size = stream.filesize
             print("Downloading the video {}...".format(self.video_title))
@@ -180,10 +187,11 @@ class PlaylistDownloader():
     """
     Base class for downloading playlists using Playlst class of pytube.
     """
+
     def __init__(self, playlist_url, type, filepath) -> None:
         """
         playlist_url = URL to the playlist on Youtube.
-        
+
         type = Download type, whether audio or video.
 
         filepath = The file location where all the videos of the playlist will be saved. In the filepath, a new directory with the playlost name would be created.
@@ -197,7 +205,6 @@ class PlaylistDownloader():
             print("Invalid playlist url!")
             quit()
 
-        
         # * initialising playlist object
         self.plt = Playlist(playlist_url)
 
@@ -221,33 +228,41 @@ class PlaylistDownloader():
                 mkdir(join_path(filepath, self.playlist_name))
                 break
             except FileExistsError:
-                new_folder_name = join_path(filepath, self.playlist_name) + "" + random_choice(alphabets)
-                print(f"Path '{join_path(filepath, self.playlist_name)}' already exists, hence making new folder with name {new_folder_name}.")
+                new_folder_name = join_path(
+                    filepath, self.playlist_name) + "" + random_choice(alphabets)
+                print(
+                    f"Path '{join_path(filepath, self.playlist_name)}' already exists, hence making new folder with name {new_folder_name}.")
                 mkdir(new_folder_name)
                 break
 
-    def download_all_videos(self, resolution:str="720p"):
+    def download_all_videos(self, resolution: str = "720p"):
         for vid in self.plt.video_urls:
             yt = YouTubeDownloader(vid, "video", self.filepath)
             yt.download_video(resolution)
 
     def download_all_audio(self):
         for vid in self.plt.video_urls():
-            yt =YouTubeDownloader(vid, "audio", self.filepath)
+            yt = YouTubeDownloader(vid, "audio", self.filepath)
             yt.download_audio()
 
 
 if __name__ == "__main__":
     print("Welcome to the Youtube Video Downloader!")
 
-    playlist_or_video = int(input("What to download? \n 1. Video \n 2. Playlist \n"))
-    
+    playlist_or_video = int(
+        input("What to download? \n 1. Video \n 2. Playlist \n"))
+
     if playlist_or_video == 1:
         url = input("Enter the video url for the video you want to download: ")
 
-        format = int(input("What do you want to download from the video:\n 1. Audio \n 2. Video \n"))
+        try:
+            format = int(
+                input("What do you want to download from the video:\n 1. Audio \n 2. Video \n"))
+        except ValueError:
+            print("Invalid input!")
 
-        filepath = input("Enter the optional file location (press enter if you dont want to specify file location): ")
+        filepath = input(
+            "Enter the optional file location (press enter if you dont want to specify file location): ")
 
         if filepath == "":
             filepath = None
@@ -258,11 +273,13 @@ if __name__ == "__main__":
             yt.download_audio()
 
         elif format == 2:
-            res = input("Enter desired resoulution for the video (example: '720p'): ").strip()
-            available_res = ["2160p", "1440p", "1080p", "720p", "480p", "360p", "240p", "144p"]
+            res = input(
+                "Enter desired resoulution for the video (example: '720p'): ").strip()
+            available_res = ["2160p", "1440p", "1080p",
+                            "720p", "480p", "360p", "240p", "144p"]
 
             if res not in available_res:
-                print("Invalid resolution! Resolutions can range in", available_res)
+                print("Invalid resolution! Resolutions can range in", " ".join(available_res))
                 quit()
 
             print("Wait while the download is being started...")
@@ -274,13 +291,15 @@ if __name__ == "__main__":
             print("Invalid input!")
             quit()
 
-
     elif playlist_or_video == 2:
-        url = input("Enter the playlist url for the playlist you want to download: ")
+        url = input(
+            "Enter the playlist url for the playlist you want to download: ")
 
-        format = int(input("What do you want to download from the playlist:\n 1. Audio \n 2. Video \n"))
+        format = int(input(
+            "What do you want to download from the playlist:\n 1. Audio \n 2. Video \n"))
 
-        filepath = input("Enter the optional file location (press enter if you dont want to specify file location): ")
+        filepath = input(
+            "Enter the optional file location (press enter if you dont want to specify file location): ")
 
         if filepath == "":
             filepath = None
@@ -291,8 +310,10 @@ if __name__ == "__main__":
             plt.download_all_audio()
 
         elif format == 2:
-            res = input("Enter desired resoulution for the video (example: '720p'): ").strip()
-            available_res = ["2160p", "1440p", "1080p", "720p", "480p", "360p", "240p", "144p"]
+            res = input(
+                "Enter desired resoulution for the video (example: '720p'): ").strip()
+            available_res = ["2160p", "1440p", "1080p",
+                             "720p", "480p", "360p", "240p", "144p"]
 
             if res not in available_res:
                 print("Invalid resolution! Resolutions can range in", available_res)
@@ -306,7 +327,6 @@ if __name__ == "__main__":
         else:
             print("Invalid input!")
             quit()
-
 
     else:
         print("Invalid input!")
